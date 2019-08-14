dcf8f5a699803b6fc2db971f6d1ca39438f1a101
Johann Hemmann <johann.hemmann@dfki.de>
2019-08-14 17:38:59 +0200

    [WIP]

--- converter.py
+++ converter.py
@@ -3,30 +3,26 @@ import os
 
 import docker
 
-from converter.utilities import get_files, get_msg_file_name
+from converter.utilities import get_files, process_file
 
 logging.basicConfig(level=logging.INFO)
 
 
 class Converter():
+    """Class to convert log- to msg-files"""
+
     client: docker.DockerClient = docker.from_env()
     image_name: str = "af01/pocolog2msgpack"
 
-    def __init__(self) -> None:
+    def __init__(self, vol_name: str = "data/") -> None:
         logging.info("Init Converter")
 
-    def convert(self, file_name: str, vol_name: str = "data/") -> None:
-        logging.info(f"Start converting {file_name}")
-
         vol_dir = os.path.abspath(vol_name)
-        msg_file_name = get_msg_file_name(file_name)
 
-        command = f"-l ./log/{file_name} -o ./msg/{msg_file_name}"
-        logging.debug(f"... CMD: {command}")
-
-        self.client.containers.run(
+        self.container = cont = self.client.containers.run(
             image=self.image_name,
-            command=command,
+            entrypoint="sleep",
+            command="infinity",
             volumes={
                 vol_dir: {
                     "bind": "/rdp",
@@ -35,39 +31,63 @@ class Converter():
             },
             working_dir="/rdp",
             remove=True,
+            detach=True,
         )
-        logging.info(f"... done")
+        logging.info(f"... done ({cont})")
 
+    def __del__(self) -> None:
+        logging.info(f"Stop and remove Converter ({self.container})")
 
-class BatchConverter():
-    conv: Converter = Converter()
+        # only STOP is needed, since REMOVE is done by container automatically
+        self.container.stop(timeout=10)
+        logging.info("... done.")
 
-    def __init__(self, base_path: str, file_type: str) -> None:
-        self.log_files = get_files(
+    def convert(self, file_name: str) -> None:
+        logging.info(f"Start converting {file_name}")
+
+        # TODO: think about solution to use e.g. base_path instead of hard-coded path-fragments
+
+        p_file = process_file(file_name, new_extension="msg")
+
+        log_file_path = f"./log/{p_file.full}"
+        msg_file_path = f"./msg/{processed_file.get('')}"
+
+        msg_file_name = extract_file_name(msg_file_path)
+        msg_file_path = f"./test/{msg_file_name}"
+
+        os.makedirs(f"./data/msg/{msg_file_path}", exist_ok=True)
+
+        command = f"bash /opt/rock/run.sh -l {log_file_path} -o {msg_file_path}"
+        logging.info(f"... CMD: {command}")
+
+        ret = self.container.exec_run(
+            cmd=command,
+        )
+        logging.info(f"... done (return code: {ret.exit_code})")
+        logging.info(f"... output:\n{ret.output.decode('utf-8')}")
+
+        return None
+
+    def convert_batch(self, base_path: str, file_type: str, cache: bool = True) -> None:
+        logging.info(f"Start converting {file_type}-files in {base_path}")
+        log_files = get_files(
             base_path=base_path,
             file_type=file_type,
             log=True,
         )
 
-        logging.info("Init BatchConverter")
-
-    def convert(self, vol_name: str = "data/", cache: bool = False) -> None:
-
-        for file_name in self.log_files:
-            if cache is True and self.msg_exists(file_name) is True:
-                logging.info(f"msg-file {msg_file_name} already exists")
+        for file_name in log_files:
+            if cache is True and self._msg_exists(file_name) is True:
+                logging.info(f"... msg-file for {file_name} already exists")
                 continue
             else:
-                self.conv.convert(
-                    file_name=file_name,
-                    vol_name=vol_name,
-                )
+                self.convert(file_name=file_name)
 
-    def msg_exists(self, file_name: str) -> bool:
+    def _msg_exists(self, file_name: str) -> bool:
         msg_files = get_files("data/msg/", file_type="msg")
-        msg_file_name = get_msg_file_name(file_name)
+        msg_file_path = get_new_extension(file_name, new_type="msg")
 
-        if msg_file_name in msg_files:
+        if msg_file_path in msg_files:
             return True
         else:
             return False
@@ -75,8 +95,12 @@ class BatchConverter():
 
 def main() -> None:
 
-    conv = BatchConverter(base_path="data/log/", file_type="log")
-    conv.convert()
+    conv = Converter()
+    conv.convert_batch(
+        base_path="data/log/20161029_FLC_test",
+        file_type="log",
+        cache=False,
+    )
 
 
 if __name__ == "__main__":
--- utilities.py
+++ utilities.py
@@ -1,3 +1,4 @@
+import collections
 import glob
 import logging
 
@@ -9,10 +10,40 @@ def get_files(base_path: str, file_type: str, log: bool = False) -> list:
     files = ["/".join(f.split("/")[2:]) for f in raw_files]
 
     if log is True:
-        logging.info(f"... found {len(files)} {file_type}-files")
+        logging.info(
+            f"... found {len(files)} {file_type}-files in {base_path}"
+        )
 
     return files
 
 
-def get_msg_file_name(file_name: str) -> bool:
-    return f"{file_name.split('/')[-1].split('.')[0]}.msg"
+def get_new_extension(file_name: str, new_type: str) -> str:
+    without_ext = ".".join(file_name.split(".")[:-1])
+    with_new_ext = f"{without_ext}.{new_type}"
+
+    return with_new_ext
+
+
+def split_path_name(file_path: str) -> dict:
+
+    path_list = file_path.split("/")
+    file_dict = {
+        "path": "/".join(path_list[:-1]),
+        "name": "/".join(path_list[-1]),
+        "full": file_path,
+    }
+
+    return file_dict
+
+
+def process_file(file_path: str, new_extension: str = "") -> dict:
+
+    file_dict = split_path_name(file_path)
+
+    if new_extension != "":
+        file_dict["old_name"] = file_dict.get("name")
+        new_name = file_dict["name"] = get_new_extension(file_name)
+
+        ret["full"] = f"{file_dict.get('path')}/{new_name}"
+
+    return file_dict
