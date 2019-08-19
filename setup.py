5153f680cf56893bfe30afd282ba920c09c2e252
Johann Hemmann <johann.hemmann@dfki.de>
2019-08-19 11:49:43 +0200

    Make procolog2msgpack installable

---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name='procolog2msgpack',
+    version='1.0.0',
+    description="""Python wrapper around procolog2msgpack,
+    to convert rock-log- to msgpack-files""",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Johann Hemmann",
+    author_email="johann.hemmann@dfki.de",
+    license="LGPL-3.0",
+    classifiers=[
+        "License :: OSI Approved :: GNU LESSER GENERAL PUBLIC LICENSE",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(
+        exclude=("convert_log_files")
+    ),
+    include_package_data=True,
+    install_requires=[
+        'msgpack',
+        'docker',
+    ],
+)
