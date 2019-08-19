30ed1754b1b154c1f15326443ab56b422c7e2345
Johann Hemmann <johann.hemmann@dfki.de>
2019-08-19 16:00:33 +0200

    Update version to 0.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name='pocolog2msgpack',
-    version='1.0.0',
+    version='0.1.0',
     description="""Python wrapper around procolog2msgpack,
     to convert rock-log- to msgpack-files""",
     long_description=README,
